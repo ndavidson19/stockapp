9f6c7c406a6e8b1e271788491445d7425ca8a150
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2022-12-20 21:47:32 -0800

    Added Simple Ensemble Model

---
+++ backend/ensemble.py
@@ -0,0 +1,31 @@
+
+
+class EnsembleModel:
+  def __init__(self):
+    self.model1 = Arima()
+    self.model2 = LSTM()
+    self.model3 = XGBOOST()
+    self.model4 = SVM()
+    self.model5 = Fourier()
+    
+  def fit(self, X_train, y_train):
+    # Train each individual model on the training data
+    self.model1.fit(X_train, y_train)
+    self.model2.fit(X_train, y_train)
+    self.model3.fit(X_train, y_train)
+    self.model4.fit(X_train, y_train)
+    self.model5.fit(X_train, y_train)
+    
+  def predict(self, X):
+    # Use each individual model to make predictions on the input data
+    y_pred1 = self.model1.predict(X)
+    y_pred2 = self.model2.predict(X)
+    y_pred3 = self.model3.predict(X)
+    y_pred4 = self.model4.predict(X)
+    y_pred5 = self.model5.predict(X)
+    
+    # Combine the predictions of the individual models in a way that suits your needs
+    y_pred = (y_pred1 + y_pred2 + y_pred3 + y_pred4 + y_pred5) / 5
+
+    return y_pred
+
