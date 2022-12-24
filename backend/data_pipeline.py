24b4a64fa5918233519f825001be19e793b9161a
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2022-12-24 15:31:47 -0800

    pipeline changes

--- backend/data_pipeline.py
+++ backend/data_pipeline.py
@@ -1,60 +1,38 @@
+import numpy as np
+
+
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from alpha_vantage.timeseries import TimeSeries 
 
 
-config = {
-    "alpha_vantage": {
-        "key": "COR04RQ1Z1P74ETF", 
-        "symbol": "SPY",
-        "outputsize": "full",
-        "key_adjusted_close": "5. adjusted close",
-    },
-    "data": {
-        "window_size": 20,
-        "train_split_size": 0.80,
-    }, 
-    "plots": {
-        "xticks_interval": 90, # show a date every 90 days
-        "color_actual": "#001f3f",
-        "color_train": "#3D9970",
-        "color_val": "#0074D9",
-        "color_pred_train": "#3D9970",
-        "color_pred_val": "#0074D9",
-        "color_pred_test": "#FF4136",
-    },
-    "model": {
-        "input_size": 1, # since we are only using 1 feature, close price
-        "num_lstm_layers": 5,
-        "lstm_size": 128,
-        "dropout": 0.2,
-    },
-    "training": {
-        "device": "cpu", # "cuda" or "cpu"
-        "batch_size": 64,
-        "num_epoch": 100,
-        "learning_rate": 0.01,
-        "scheduler_step_size": 40,
-    }
-}
+class DataPipeline(Dataset):
+    def __init__(self, config):
+        self.config = config
+        self.data_date, self.data_close_price, self.num_data_points, self.display_date_range = self.download_data()
+        self.normalized_data_close_price = self.normalize()
+        self.data_x, self.data_x_unseen = self.prepare_data_x()
+        self.data_y = self.prepare_data_y()
+        self.data_x_train, self.data_x_val, self.data_y_train, self.data_y_val = self.split_data()
+        self.to_plot_data_y_train, self.to_plot_data_y_val = self.prepare_data_for_plotting()
 
-def download_data(config):
-    ts = TimeSeries(key='COR04RQ1Z1P74ETF') #you can use the demo API key for this project, but please make sure to eventually get your own API key at https://www.alphavantage.co/support/#api-key. 
-    data, meta_data = ts.get_daily_adjusted(config["alpha_vantage"]["symbol"], outputsize=config["alpha_vantage"]["outputsize"])
+    def download_data(self):
+        config = self.config
+        ts = TimeSeries(key='COR04RQ1Z1P74ETF') #you can use the demo API key for this project, but please make sure to eventually get your own API key at https://www.alphavantage.co/support/#api-key. 
+        data, meta_data = ts.get_daily_adjusted(config["alpha_vantage"]["symbol"], outputsize=config["alpha_vantage"]["outputsize"])
 
-    data_date = [date for date in data.keys()]
-    data_date.reverse()
+        data_date = [date for date in data.keys()]
+        data_date.reverse()
 
-    data_close_price = [float(data[date][config["alpha_vantage"]["key_adjusted_close"]]) for date in data.keys()]
-    data_close_price.reverse()
-    data_close_price = np.array(data_close_price)
+        data_close_price = [float(data[date][config["alpha_vantage"]["key_adjusted_close"]]) for date in data.keys()]
+        data_close_price.reverse()
+        data_close_price = np.array(data_close_price)
 
-    num_data_points = len(data_date)
-    display_date_range = "from " + data_date[0] + " to " + data_date[num_data_points-1]
-    print("Number data points", num_data_points, display_date_range)
+        num_data_points = len(data_date)
+        display_date_range = "from " + data_date[0] + " to " + data_date[num_data_points-1]
+        print("Number data points", num_data_points, display_date_range)
 
-    return data_date, data_close_price, num_data_points, display_date_range
-
-data_date, data_close_price, num_data_points, display_date_range = download_data(config)
+        return data_date, data_close_price, num_data_points, display_date_range
 
 class Normalizer():
     def __init__(self):
@@ -75,44 +53,64 @@ scaler = Normalizer()
 normalized_data_close_price = scaler.fit_transform(data_close_price)
 
 
+class DataCleaning():
+    def __init__(self):
+        self.data_x = None
+        self.data_x_unseen = None
+        self.data_y = None
+        self.data_x_train = None
+        self.data_x_val = None
+        self.data_y_train = None
+        self.data_y_val = None
+        self.to_plot_data_y_train = None
+        self.to_plot_data_y_val = None
 
-def prepare_data_x(x, window_size):
-    # perform windowing
-    n_row = x.shape[0] - window_size + 1
-    output = np.lib.stride_tricks.as_strided(x, shape=(n_row, window_size), strides=(x.strides[0], x.strides[0]))
-    return output[:-1], output[-1]
+    def prepare_data_x(self, x, window_size):
+        # perform windowing
+        n_row = x.shape[0] - window_size + 1
+        output = np.lib.stride_tricks.as_strided(x, shape=(n_row, window_size), strides=(x.strides[0], x.strides[0]))
+        return output[:-1], output[-1]
+
+    def prepare_data_y(self, x, window_size):
+        # # perform simple moving average
+        # output = np.convolve(x, np.ones(window_size), 'valid') / window_size
+
+        # use the next day as label
+        output = x[window_size:]
+        return output
+
+    def split_data(self, data_x, data_y, train_split_size):
+        split_index = int(data_y.shape[0]*train_split_size)
+        data_x_train = data_x[:split_index]
+        data_x_val = data_x[split_index:]
+        data_y_train = data_y[:split_index]
+        data_y_val = data_y[split_index:]
+        return data_x_train, data_x_val, data_y_train, data_y_val
+
+    def prepare_data_for_plotting(self, num_data_points, data_y_train, data_y_val):
+        to_plot_data_y_train = np.zeros(num_data_points)
+        to_plot_data_y_val = np.zeros(num_data_points)
+
+        to_plot_data_y_train[config["data"]["window_size"]:] = data_y_train
+        to_plot_data_y_val[config["data"]["window_size"]:split_index] = data_y_train[-1]
+        to_plot_data_y_val[split_index:] = data_y_val
+        return to_plot_data_y_train, to_plot_data_y_val
+
+    def get_data(self):
+        return self.data_x, self.data_x_unseen, self.data_y, self.data_x_train, self.data_x_val, self.data_y_train, self.data_y_val, self.to_plot_data_y_train, self.to_plot_data_y_val
+
+    def run(self, config, normalized_data_close_price):
+        self.data_x, self.data_x_unseen = self.prepare_data_x(normalized_data_close_price, window_size=config["data"]["window_size"])
+        self.data_y = self.prepare_data_y(normalized_data_close_price, window_size=config["data"]["window_size"])
+        self.data_x_train, self.data_x_val, self.data_y_train, self.data_y_val = self.split_data(self.data_x, self.data_y, train_split_size=config["data"]["train_split_size"])
+        self.to_plot_data_y_train, self.to_plot_data_y_val = self.prepare_data_for_plotting(num_data_points, self.data_y_train, self.data_y_val)
 
 
-def prepare_data_y(x, window_size):
-    # # perform simple moving average
-    # output = np.convolve(x, np.ones(window_size), 'valid') / window_size
+data_cleaning = DataCleaning()
 
-    # use the next day as label
-    output = x[window_size:]
-    return output
+data_cleaning.run(config, normalized_data_close_price)
 
-data_x, data_x_unseen = prepare_data_x(normalized_data_close_price, window_size=config["data"]["window_size"])
-data_y = prepare_data_y(normalized_data_close_price, window_size=config["data"]["window_size"])
-
-# split dataset
-
-split_index = int(data_y.shape[0]*config["data"]["train_split_size"])
-data_x_train = data_x[:split_index]
-data_x_val = data_x[split_index:]
-data_y_train = data_y[:split_index]
-data_y_val = data_y[split_index:]
-
-# prepare data for plotting
-
-to_plot_data_y_train = np.zeros(num_data_points)
-to_plot_data_y_val = np.zeros(num_data_points)
-
-to_plot_data_y_train[config["data"]["window_size"]:split_index+config["data"]["window_size"]] = scaler.inverse_transform(data_y_train)
-to_plot_data_y_val[split_index+config["data"]["window_size"]:] = scaler.inverse_transform(data_y_val)
-
-to_plot_data_y_train = np.where(to_plot_data_y_train == 0, None, to_plot_data_y_train)
-to_plot_data_y_val = np.where(to_plot_data_y_val == 0, None, to_plot_data_y_val)
-
+data_x, data_x_unseen, data_y, data_x_train, data_x_val, data_y_train, data_y_val, to_plot_data_y_train, to_plot_data_y_val = data_cleaning.get_data()
 
 class TimeSeriesDataset(Dataset):
     def __init__(self, x, y):
@@ -128,9 +126,6 @@ class TimeSeriesDataset(Dataset):
 
 dataset_train = TimeSeriesDataset(data_x_train, data_y_train)
 dataset_val = TimeSeriesDataset(data_x_val, data_y_val)
-
-print("Train data shape", dataset_train.x.shape, dataset_train.y.shape)
-print("Validation data shape", dataset_val.x.shape, dataset_val.y.shape)
 
 train_dataloader = DataLoader(dataset_train, batch_size=config["training"]["batch_size"], shuffle=True)
 val_dataloader = DataLoader(dataset_val, batch_size=config["training"]["batch_size"], shuffle=True)
