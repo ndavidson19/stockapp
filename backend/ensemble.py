5bc6c1c1bf633122deda9619d14d1bc3f888aa9c
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2023-02-06 19:29:32 -0800

    Fixed XGB regressor

--- backend/ensemble.py
+++ backend/ensemble.py
@@ -14,9 +14,9 @@ class EnsembleModel:
     self.ARIMA = ARIMA(config)
     self.LSTM = LSTMModel()
     self.XGB = XGBOOST(config)
-    #self.model4 = SVM()
+    self.SVM = SVM()
     self.FOURIER = Fourier()
-    self.model6 = PolyReg()
+    self.POLY = PolyReg()
     self.config = config
     
   def fit(self, training_data, validation_data, training_data_1d, validation_data_1d):
@@ -26,19 +26,19 @@ class EnsembleModel:
     #self.ARIMA.fit(training_data_1d.x) # no y_train for ARIMA
     #self.LSTM.fit(model = self.LSTM, X_train = training_data.x, y_train = training_data.y, X_val=validation_data.x, y_val = validation_data.y)
     self.XGB.fit(X_train  = training_data_1d.x, y_train = training_data_1d.y, X_val = validation_data_1d.x, y_val = validation_data_1d.y)
-    #self.model4.fit(training_data.x, training_data.y)
+    self.SVM.fit(training_data.x, training_data.y)
     self.FOURIER.fit(training_data_1d.x, training_data_1d.y)
-    self.model6.fit(training_data_1d.x, training_data_1d.y)
+    self.POLY.fit(training_data_1d.x, training_data_1d.y)
 
     
   def predict(self, validation_data, validation_data_1d, validation_dataloader, x_unseen):
     # Use each individual model to make predictions on the input data
-    y_pred1 = self.ARIMA.forecast(validation_data_1d, n_steps=20)
+    #y_pred1 = self.ARIMA.forecast(validation_data_1d, n_steps=20)
     y_pred2 = self.LSTM.predict(test_dataloader=validation_dataloader, x_unseen=x_unseen)
     y_pred3 = self.XGB.predict()
     #y_pred4 = self.model4.predict(X)
     y_pred5 = self.FOURIER.predict(validation_data.y)
-    y_pred6 = self.model6.eval(validation_data_1d.x, validation_data_1d.y)
+    y_pred6 = self.POLY.eval(validation_data_1d.x, validation_data_1d.y)
     
     # Combine the predictions of the individual models in a way that suits your needs
     #y_pred = (y_pred1 + y_pred2 + y_pred3 + y_pred4 + y_pred5 + y_pred6) / 6
@@ -47,5 +47,5 @@ class EnsembleModel:
 
    
 
-    return y_pred1, y_pred2, y_pred5, y_pred6
+    return y_pred2, y_pred5, y_pred6
 
